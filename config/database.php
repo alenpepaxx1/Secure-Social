b97ad655fe7a1ab168f67f83ddfbe0cca4b7f93e
Alen Pepa <xalenpepa@gmail.com>
2026-04-02 14:58:08 -0700

    Create database.php

---
+++ config/database.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Copyright Alen Pepa
+ * All rights reserved.
+ */
+
+return [
+    'host'     => 'localhost',
+    'dbname'   => 'alen',
+    'user'     => 'alen',
+    'password' => 'test2026',
+    'charset'  => 'utf8mb4'
+];
